4d9955d5817678e8ff0e3cc891b4e023de2bfce2
RikRodel1997 <145914563+RikRodel1997@users.noreply.github.com>
2024-06-30 20:37:04 +0200

    Finished Python client /echo endpoint test and added unit test for echo tail extraction

--- src/c/__test__/check_request.c
+++ src/c/__test__/check_request.c
@@ -29,19 +29,33 @@ END_TEST
 START_TEST(test_valid_method_false) { ck_assert_int_eq(valid_method("PATCH"), 0); }
 END_TEST
 
+START_TEST(test_echo_tail_exists) {
+    char tail[6];
+    get_echo_tail("/echo/TAIL12", tail);
+    ck_assert_str_eq(tail, "TAIL12");
+}
+END_TEST
+
+START_TEST(test_echo_tail_doesnt_exists) {
+    char tail[6];
+    get_echo_tail("/echo/", tail);
+    ck_assert_str_eq(tail, "");
+}
+END_TEST
+
 Suite* request_suite(void) {
     Suite* s;
     TCase* tc_core;
 
     s = suite_create("Request");
-
-    /* Core test case */
     tc_core = tcase_create("Core");
 
     tcase_add_test(tc_core, test_parse_request_valid);
     tcase_add_test(tc_core, test_parse_request_invalid_method);
     tcase_add_test(tc_core, test_valid_method_true);
     tcase_add_test(tc_core, test_valid_method_false);
+    tcase_add_test(tc_core, test_echo_tail_exists);
+    tcase_add_test(tc_core, test_echo_tail_doesnt_exists);
     suite_add_tcase(s, tc_core);
 
     return s;
--- tests.py
+++ tests.py
@@ -14,5 +14,11 @@ def test_home() -> bool:
 
 
 def test_echo() -> bool:
-    response = curl_request(f"{BASE_URL}/echo/{random_string(6)}")
-    print(response)
+    string_data = random_string(6)
+    response = curl_request(f"{BASE_URL}/echo/{string_data}")
+    parts = response.split("\n")
+    assert_that(parts[0]).is_equal_to("HTTP/1.1 200 OK")
+    assert_that(parts[1]).is_equal_to("Content-Type: text/plain")
+    assert_that(parts[2]).is_equal_to("Content-Length: 6")
+    assert_that(parts[4]).is_equal_to(string_data)
+    return True
